deda232fe39f3f87e564e0ea1401d80b2a7bfcee
Saurabh Raje <saurabh.mraje@gmail.com>
2016-07-19 00:24:34 +0530

    v1.0
    Major updates:
    1. Optimised allocation and returning functions.
    2. Implemented error handling
    3. Made allocation and return thread safe.

--- mempool.c
+++ mempool.c
@@ -1,81 +1,134 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+#include <assert.h>
+
 #define POOLSIZE 20
 
+enum interrorcodes{ALLOK = 0, NOTFOUND = 1, MALLOCFAIL = 2, POOLDEAD = 3, MEMSETFAIL = 4};
+//enum ptrerrorcodes{BUFFERFULL = NULL, DEADPOOL = NULL};						//See what can be done
+
+pthread_mutex_t locks[POOLSIZE];
+
 typedef struct{
-int* ptr;
+void* ptr;
 int flag;
 } chunk;
 
-chunk* init_pool(int blocksize)
+static chunk arr[POOLSIZE];														//static ensures that scope of this variable is limited to this file. Similar to private. Hence extern arr[] will not work in calling functions in separate file.
+
+int init_pool(int blocksize)													//No need for thread-safe as this should be called before forking
 {
-	static chunk arr[POOLSIZE];
-	
-	int i = 0;
-	
-	for(; i<POOLSIZE; i++)
+	int j;
+	for(j = 0; j<POOLSIZE; j++)
 	{
-		arr[i].ptr = (int*) malloc(blocksize);
+		pthread_mutex_init(&(locks[j]), 0);	
+	}
+		
+	assert(blocksize>0);
+	
+	int i;
+	
+	for(i = 0; i<POOLSIZE; i++)
+	{
+		arr[i].ptr = (void*) malloc(blocksize);
+		if(arr[i].ptr == NULL)
+		{
+			return MALLOCFAIL;
+		}
 		arr[i].flag = 0;
 	}
 	
-	return arr;
+	return ALLOK;
 }
 
-int* alloc(chunk* pool)
+void* alloc()
 {
-	int i = 0;
+	int i;
 	
-	for(; i<POOLSIZE; i++)
+	for(i = 0; i<POOLSIZE; i++)
 	{
-		if(!pool[i].flag)
-		{
-			break;
-		}
+		pthread_mutex_lock(&(locks[i]));
+			if((arr+i) == NULL)
+			{
+				return NULL;
+			}
+		
+			if(!arr[i].flag)
+			{
+				pthread_mutex_unlock(&(locks[i]));
+				break;
+			}
+		pthread_mutex_unlock(&(locks[i]));
 	}
 	
-	pool[i].flag = 1;
+	if(i==POOLSIZE)
+	{
+		return NULL;
+	}
 	
-	return pool[i].ptr;
+	pthread_mutex_lock(&(locks[i]));
+		arr[i].flag = 1;
+	pthread_mutex_unlock(&(locks[i]));
+	
+	return arr[i].ptr;
 }
 
-void ret(int* pt, chunk* ch, int blocksize)
+int ret(void* pt, int blocksize)
 {
-	int i = 0;
+	int i;
 	
-	for(; i<POOLSIZE; i++)
+	for(i = 0; i<POOLSIZE; i++)
 	{
-		if(pt==ch[i].ptr)
+	pthread_mutex_lock(&(locks[i]));
+		if((arr+i) == NULL)
 		{
-			free(ch[i].ptr);
-			ch[i].ptr = (int *) malloc(blocksize);
-			ch[i].flag = 0;
-			break;
+			pthread_mutex_unlock(&(locks[i]));
+			return POOLDEAD;
 		}
+		
+		if(pt==arr[i].ptr)
+		{
+			arr[i].ptr = memset(arr[i].ptr, 0, blocksize); 
+			if((arr+i) == NULL)
+			{
+				pthread_mutex_unlock(&(locks[i]));
+				return MEMSETFAIL;
+			}
+			arr[i].flag = 0;
+			pthread_mutex_unlock(&(locks[i]));
+			return ALLOK;
+		}
+	pthread_mutex_unlock(&(locks[i]));
+	}
+	
+	if(i==POOLSIZE)
+	{
+		return NOTFOUND;														//See what can be done if this is returned?
 	}
 }
 
-int main()
+/*int main()
 {
-	chunk *c = init_pool(8);
-	int dummy[10000];
+	init_pool(8);
+	//int dummy[10000];
 	int j = 0;
 	int* i[40];
 	
 	for(; j<20; j++)
 	{
-		i[j] = alloc(c);
+		i[j] = (int *) alloc();
 		*(i[j]) = 10000*j;
 		printf("%d\n", *(i[j]));
 	}
 	
 	for(; j<40; j++)
 	{
-		ret(i[j-20], c, 8);
-		i[j] = alloc(c);
-		*(i[j]) = 2147483647;
+		ret(i[j-20], 8);
+		i[j] = (int *) alloc();
+		//*(i[j]) = 2147483647;  
 		printf("%d\n", *(i[j]));
 	}
 	
-}
-
+}*/
