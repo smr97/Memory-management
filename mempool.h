b2d39ee056d8a2ef644f3cc633d4db6304eaef85
Saurabh Raje <saurabh.mraje@gmail.com>
2016-07-21 19:12:54 +0530

    v0.1
    Updated the header to work with v1.2 of mempool.c

--- mempool.h
+++ mempool.h
@@ -2,12 +2,6 @@
 #define mempool.h
 
 enum interrorcodes{ALLOK = 0, NOTFOUND = 1, MALLOCFAIL = 2, POOLDEAD = 3, MEMSETFAIL = 4};
-//enum ptrerrorcodes{BUFFERFULL = NULL, DEADPOOL = NULL};
-
-typedef struct{
-int* ptr;
-int flag;
-} chunk;
 
 int init_pool(int blocksize);
 
